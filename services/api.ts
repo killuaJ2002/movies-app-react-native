8fd87247619b16793cc0a53fb03bbe6b410f1195
killuaJ2002 <kjcodes2002@gmail.com>
2025-10-17 03:29:00 +0530

    setup api configs

---
+++ services/api.ts
@@ -0,0 +1,40 @@
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
+
+export const fetchPopularMovies = async ({query}: {query: string}) => {
+    const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    });
+
+    if(!response.ok) {
+        throw new Error('Failed to fetch movie');
+    }
+
+    const data = await response.json();
+    return data.results;
+}
+
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjZiYmZiMzJlY2JkMjRmNjg3ZjA1YzdmZDc3YTdlZSIsIm5iZiI6MTc2MDY0OTk1My41MjE5OTk4LCJzdWIiOiI2OGYxNjJlMThhOTRmMDBmMTc2YzZjY2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.LIH69on-GqLPkrnDle8NeiXuv4KtAPhY9JL3N5xZTMM'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
